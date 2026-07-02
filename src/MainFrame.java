cbbf45395e05760f970d8bbaf1dc81de9c6fd44d
Rosmeri <rosmerisimatupang443@gmail.com>
2026-07-02 09:34:54 +0700

    Commit awal: Kode legacy dari dosen

---
+++ src/MainFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+        setTitle("Legacy CRUD Mahasiswa");
+        setSize(900, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel("Legacy CRUD Mahasiswa", SwingConstants.CENTER);
+        add(label);
+    }
+}
